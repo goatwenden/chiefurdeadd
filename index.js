ef3ab5b02bb9967f9fe0d15e8879f31c22c92274
goatwenden <kareembabban11@gmail.com>
2026-07-11 00:43:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-
 const axios = require('axios');
 const http = require('http');
 const ws = require('ws');
 
 const TOKEN = process.env.TOKEN;
 const BASE_URL = 'https://discord.com/api/v9';
-const USER_ID = '1520682184241975429';
-const TARGET_ID = '1446641976538697759';
+const USER_ID = '1522918155444551701';
+const TARGET_ID = '1479204404262473845';
 
 let renameActive = false;
 let renameInterval = null;
@@ -20,76 +19,76 @@ let targetChannel = null;
 
 const messages = [
     '@target\nfight\nme\nfaggot',
-    'yo\nvirtus\nur\nworthless\n@target',
+    'yo\nchief\nur\nworthless\n@target',
     'garbo @target',
-    'weak\nass\nvirgin\nass\nnigga\nnamed\nvirtus\nur\nmiserable\nas\nshit\nfucking\nfaggot',
-    'yo virtus\nur\na\nfucking\nfaggot\n@target',
+    'weak\nass\nvirgin\nass\nnigga\nnamed\nchief\nur\nmiserable\nas\nshit\nfucking\nfaggot',
+    'yo chief\nur\na\nfucking\nfaggot\n@target',
     'cringe faggot @target',
     'horrible as fuck @target',
-    'yo virtus ur a loser @target',
-    'yo virtus @target bitch',
+    'yo chief ur a loser @target',
+    'yo chief @target bitch',
     'cross\neyed\npedo\nass\nchild\nrapist\n@target',
-    'yo virtus where u go',
-    'yo virtus ur fucking ass @target',
-    'shut\nthe\nfuck\nup\nright\nnow\npussy\nwake\nup\nvirtus\nloooool',
-    'lame\nass\nweak\nnigga\nvirtus\nur\na\nfucking\nfaggot',
+    'yo chief where u go',
+    'yo chief ur fucking ass @target',
+    'shut\nthe\nfuck\nup\nright\nnow\npussy\nwake\nup\nchief\nloooool',
+    'lame\nass\nweak\nnigga\nchief\nur\na\nfucking\nfaggot',
     'bitch @target',
-    'virtus\nur\nfucking horrible\n@target',
-    'shit\neater\nvirtus\n@target',
-    'yo virtus ur a loser @target',
+    'chief\nur\nfucking horrible\n@target',
+    'shit\neater\nchief\n@target',
+    'yo chief ur a loser @target',
     'LOOOL',
     'faggot @target',
-    'virtus @target',
-    'yo\nvirtus\nshow\nme\nur\nwrist\nright\nnow\nshitty\nass\nfaggot',
+    'chief @target',
+    'yo\nchief\nshow\nme\nur\nwrist\nright\nnow\nshitty\nass\nfaggot',
     'LOOOOOOOOL',
-    'virtus shut up pedo',
-    'pedo virtus @target',
-    'sorry\nass\nnigga\nvirtus\nur\nnot\nsafe\nfrom\nme\nweak\nvirgin\nass\nnigga',
-    'virtus\nwake\nup\nand\nfight\nback\nfaggot\n@target',
-    'yo virtus\nu\nstill\na\nprostitute\n@target',
-    'yo\nvirtus\nur\nass\nshut\nthe\nfuck\nup\n@target',
-    'virtus the com whore @target',
-    'virtus aint u a pedo @target',
+    'chief shut up pedo',
+    'pedo chief @target',
+    'sorry\nass\nnigga\nchief\nur\nnot\nsafe\nfrom\nme\nweak\nvirgin\nass\nnigga',
+    'chief\nwake\nup\nand\nfight\nback\nfaggot\n@target',
+    'yo chief\nu\nstill\na\nprostitute\n@target',
+    'yo\nchief\nur\nass\nshut\nthe\nfuck\nup\n@target',
+    'chief the com whore @target',
+    'chief aint u a pedo @target',
     'how u been pedo @target',
     'long time no see @target',
-    'yo virtus @target',
-    'yo\nvirtus\nshut\nthe\nfuck\nup\nwhore\nur\nbreasts\nfuckign\nstink\nnigga',
-    'ur ass bitch @target\npedo named virtus',
-    'virtus shut the fuck up pedo',
-    'weak\nass\nvirgin\nnigga\nur\nmiserable\nas\nshit\nfucking\nfaggot\nvirtus',
+    'yo chief @target',
+    'yo\nchief\nshut\nthe\nfuck\nup\nwhore\nur\nbreasts\nfuckign\nstink\nnigga',
+    'ur ass bitch @target\npedo named chief',
+    'chief shut the fuck up pedo',
+    'weak\nass\nvirgin\nnigga\nur\nmiserable\nas\nshit\nfucking\nfaggot\nchief',
     'harmless @target',
     'faggot ass @target',
     'absolute loser @target',
-    'virtus ur pussy @target',
-    'pedophile\nass\nchild\nrapist\nnigga\nnamed\nvirtus\nfaggot',
+    'chief ur pussy @target',
+    'pedophile\nass\nchild\nrapist\nnigga\nnamed\nchief\nfaggot',
     'loser\nass\nnigga\nur\ntrash\n@target',
     'shitty dork ass nigga @target',
     'weak\nass\nnigga\nshut\nthe\nfuck\nup\nur\nshit\nfaggot',
     'wake\nup\nand\nshow\nme\nyour\nwrist\nright\nthe\nfuck\nnow\n@target',
     'zoophile @target',
     'necrophiliac @target',
-    'weak\nass\nnigga\nvirtus\nshut\nthe\nfuck\nup\nur\nshit\nfaggot\nLOOOL',
-    'wake\nup\nand\nfight\nback\nvirtus\nfaggot',
-    'yo btw virtus ur actually shitty @target',
+    'weak\nass\nnigga\nchief\nshut\nthe\nfuck\nup\nur\nshit\nfaggot\nLOOOL',
+    'wake\nup\nand\nfight\nback\nchief\nfaggot',
+    'yo btw chief ur actually shitty @target',
     'how u been pedo @target',
     'ur\nembarrassing\nurself\n@target',
-    'yo virtus\n@target\ndisgusting\npedo',
-    'faggot named virtus @target',
-    'virtus\nur\nfucking horrible\n@target',
-    'virtus shut the fuck up pedo @target',
-    'wsg pedo virtus',
+    'yo chief\n@target\ndisgusting\npedo',
+    'faggot named chief @target',
+    'chief\nur\nfucking horrible\n@target',
+    'chief shut the fuck up pedo @target',
+    'wsg pedo chief',
     'shitbreast @target',
-    'pedo virtus @target',
-    'nigga\nvirtus\nur\nass\nshut\nthe\nfuck\nup\nwhore\nur\nbreasts\nfucking\nstink\nloser\nass\nnigga\nLOOOL',
+    'pedo chief @target',
+    'nigga\nchief\nur\nass\nshut\nthe\nfuck\nup\nwhore\nur\nbreasts\nfucking\nstink\nloser\nass\nnigga\nLOOOL',
     'ur unloved @target',
     'social reject @target',
-    'virtus\nthe\ncutslut\n@target',
-    'virtus @target',
+    'chief\nthe\ncutslut\n@target',
+    'chief @target',
     'who asked faggot @target',
     'yo bitch @target',
-    'stupid\nfaggot\nnamed\nvirtus\n@target',
+    'stupid\nfaggot\nnamed\nchief\n@target',
     'ur a queer @target',
-    'yo virtus where u go pedo @target'
+    'yo chief where u go pedo @target'
 ];
 
 const PORT = process.env.PORT || 10000;
@@ -154,7 +153,7 @@ WebSocket.on('message', async (data) => {
             if (t === 'MESSAGE_CREATE') {
                 const msg = d;
 
-                if (msg.author.id === USER_ID && !msg.content.startsWith('!goatwenden') && !msg.content.startsWith('$killvirtus') && !msg.content.startsWith('$stopkillvirtus')) {
+                if (msg.author.id === USER_ID && !msg.content.startsWith('!goatwenden') && !msg.content.startsWith('$killchief') && !msg.content.startsWith('$stopkillchief')) {
                     axios.put(
                         `${BASE_URL}/channels/${msg.channel_id}/messages/${msg.id}/reactions/%F0%9F%94%AA/%40me`,
                         {},
@@ -186,7 +185,7 @@ WebSocket.on('message', async (data) => {
                         renameInterval = setInterval(async () => {
                             if (!renameActive || !targetGC) return;
                             try {
-                                await axios.patch(`${BASE_URL}/channels/${targetGC}`, { name: `kick=fold, justiceK, virtusK, federalK ${renameCount}` }, { headers: { Authorization: TOKEN } });
+                                await axios.patch(`${BASE_URL}/channels/${targetGC}`, { name: `kick=fold, justiceK, chiefK, federalK ${renameCount}` }, { headers: { Authorization: TOKEN } });
                                 console.log(`Renamed: ${renameCount}`);
                                 renameCount++;
                             } catch (e) { console.log('Rename failed, retrying...'); }
@@ -204,7 +203,7 @@ WebSocket.on('message', async (data) => {
                 }
 
                 // Spam loop
-                if (msg.author.id === USER_ID && msg.content === '$killvirtus') {
+                if (msg.author.id === USER_ID && msg.content === '$killchief') {
                     try { await axios.delete(`${BASE_URL}/channels/${msg.channel_id}/messages/${msg.id}`, { headers: { Authorization: TOKEN } }); } catch (e) {}
 
                     targetChannel = msg.channel_id;
@@ -226,7 +225,7 @@ WebSocket.on('message', async (data) => {
                     }, 1500);
                 }
 
-                if (msg.author.id === USER_ID && msg.content === '$stopkillvirtus') {
+                if (msg.author.id === USER_ID && msg.content === '$stopkillchief') {
                     try { await axios.delete(`${BASE_URL}/channels/${msg.channel_id}/messages/${msg.id}`, { headers: { Authorization: TOKEN } }); } catch (e) {}
                     spamActive = false;
                     if (spamInterval) clearInterval(spamInterval);
